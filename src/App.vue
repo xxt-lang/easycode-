a1feb1530bc5f354640e46e80aa0e2e307ed693d
xuxt <2286621910@qq.com>
2023-05-30 18:35:27 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    </div>
+</template>
+ <script>
+    export default {
+    name:'login',
+    props:{},
+    data () {
+        return {}
+    },
+    methods:{}
+    }
+</script>
+<style scoped>
+</style>
