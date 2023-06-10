fbc12211158455986c9660fc096838bbbfda8db8
xuxt <2286621910@qq.com>
2023-06-10 14:29:03 +0800

    预览页面

---
+++ src/views/preview.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <component
+      v-for="(item,index) in getStore('PageComponentsStore').pageComponents"
+      :is="item.component"
+      :key="index"
+      :propValue="item"
+      :index = "index"
+  />
+</div>
+</template>
+
+<script>
+import {getStore} from "../utils/core";
+
+export default {
+  name: "preview",
+  methods:{
+    getStore
+  }
+}
+</script>
+
+<style scoped>
+</style>
