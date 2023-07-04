48e00a133d642ce2d87afc71e8d24ab1c7039413
xuxt <2286621910@qq.com>
2023-07-04 19:18:35 +0800

    增加滚动条组件

--- src/utils/registered/component.js
+++ src/utils/registered/component.js
@@ -336,6 +336,15 @@ export const componentList = [
         styles: {},
     },
     {
+        component: "ScScrollbar",
+        label: '滚动条', // 左侧组件列表中显示的名字
+        type: "container",
+        animations: [], // 动画列表
+        events: {}, // 事件列表
+        attributes: {},
+        styles: {},
+    },
+    {
         component: "ScInput",
         label: '输入框',
         events: {},
@@ -893,6 +902,86 @@ export const componentSetters = [
             events: [
                 {
                     event: "click", // 事件名称
+                    enable: false,// 是否启用
+                    method: ''// 绑定方法名
+                }
+            ],
+        },
+    },
+    {
+        component: "ScScrollbar", //组件名 与组件列表中的组件一致
+        setter: {
+            attributes: [
+                {
+                    attributeName: "height",//组件配置中属性字段名
+                    label: "滚动条高度",
+                    type: "input",//编辑自段的类型input select number switch buttonList
+                    value: "400px",//属性值
+                    defaultValue: "400px",//默认属性值
+                },
+                {
+                    attributeName: "max-height",//组件配置中属性字段名
+                    label: "滚动条最大高度",
+                    type: "input",//编辑自段的类型input select number switch buttonList
+                    value: "",//属性值
+                    defaultValue: "",//默认属性值
+                },
+                {
+                    attributeName: "native",//组件配置中属性字段名
+                    label: "原生滚动条样式",
+                    type: "switch",//编辑自段的类型input select number switch buttonList
+                    value: false,//属性值
+                    defaultValue: false,//默认属性值
+                },
+                {
+                    attributeName: "min-size",//组件配置中属性字段名
+                    label: "滚动条最小尺寸",
+                    type: "inputNumber",//编辑自段的类型input select number switch buttonList
+                    value: 20,//属性值
+                    defaultValue: 20,//默认属性值
+                    max:999
+                },
+                {
+                    attributeName: "noresize",//组件配置中属性字段名
+                    label: "不响应容器尺寸变化",
+                    type: "switch",//编辑自段的类型input select number switch buttonList
+                    value: false,//属性值
+                    defaultValue: false,//默认属性值
+                },
+                {
+                    attributeName: "always",//组件配置中属性字段名
+                    label: "滚动条总是显示",
+                    type: "switch",//编辑自段的类型input select number switch buttonList
+                    value: false,//属性值
+                    defaultValue: false,//默认属性值
+                },
+                {
+                    attributeName: "tag",//组件配置中属性字段名
+                    label: "视图的元素标签",
+                    type: "input",//编辑自段的类型input select number switch buttonList
+                    value: "div",//属性值
+                    defaultValue: "div",//默认属性值
+                },
+                {
+                    attributeName: "ref",//组件配置中属性字段名
+                    label: "ref",
+                    type: "input",//编辑自段的类型input select number switch buttonList
+                    value: "link",//属性值
+                    defaultValue: "link",//默认属性值
+                },
+                {
+                    attributeName: "innerRef",//组件配置中属性字段名
+                    label: "innerRef",
+                    type: "input",//编辑自段的类型input select number switch buttonList
+                    value: "",//属性值
+                    defaultValue: "",//默认属性值
+                },
+
+            ],
+            styles: {},
+            events: [
+                {
+                    event: "scroll", // 事件名称
                     enable: false,// 是否启用
                     method: ''// 绑定方法名
                 }
--- src/views/RegisteredComponent/SCElementUI/ScScrollbar.vue
+++ src/views/RegisteredComponent/SCElementUI/ScScrollbar.vue
@@ -1,12 +1,40 @@
 <template>
-  <el-scrollbar height="400px" :ref="propValue.attributes['ref']">
-    <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+  <el-scrollbar :height="propValue.attributes['height']"
+                :ref="propValue.attributes['ref']"
+                :max-height = "propValue.attributes['max-height']"
+                :native = "propValue.attributes['native']"
+                :wrap-style = "propValue.attributes['wrap-style']"
+                :wrap-class = "propValue.attributes['wrap-class']"
+                :view-style = "propValue.attributes['view-style']"
+                :view-class = "propValue.attributes['view-class']"
+                :noresize = "propValue.attributes['noresize']"
+                :tag = "propValue.attributes['tag']"
+                :always = "propValue.attributes['always']"
+                :min-size = "propValue.attributes['min-size']"
+                @scroll = "scroll"
+  >
+    <div :ref="propValue.attributes['innerRef']">
+      <Container
+          key="editorContainer"
+          :container="{
+          id:propValue.id,
+          featherId:propValue.featherId,
+          children:propValue.children,
+          componentId:propValue.id,
+          status:propValue.status,}"
+          :container-styles = "propValue.styles"
+          :isPreview = "isPreview"
+          :EcVue = "EcVue"
+      ></Container>
+    </div>
   </el-scrollbar>
 </template>
 
 <script>
+import Container from "../../coreComponents/Container.vue";
 export default {
   name: "ScScrollbar",
+  components:{Container},
   props:{
     propValue: {
       type: Object, String,
@@ -22,12 +50,33 @@ export default {
       default:()=>{}
     }
   },
-  mounted:{
-
+  mounted() {
+    this.EcVue.$refs[this.propValue.attributes['ref'] ] = this.$refs[this.propValue.attributes['ref'] ]
+    this.EcVue.$refs[this.propValue.attributes['innerRef'] ] = this.$refs[this.propValue.attributes['innerRef'] ]
+  },
+  methods:{
+    scroll(scrollLeft,scrollTop){
+      if(this.propValue.events['click'].enable){
+        this.EcVue[this.propValue.events['click'].method](scrollLeft,scrollTop)
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+.el-slider {
+  margin-top: 20px;
+}
 </style>
